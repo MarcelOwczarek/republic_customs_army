c7269eac33924463e66629226e3bfb5ba74a37d0
Marcel <owczarek2000@gmail.com>
2023-02-10 18:01:37 +0100

    Add GridView to ShopPage

--- lib/app/ShopPage.dart
+++ lib/app/ShopPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:republic_customs_army/app/SecondLoginPage.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({
@@ -7,8 +8,50 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Jeden'),
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: const EdgeInsets.all(10),
+      children: [
+        Container(
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.all(5),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            image: DecorationImage(
+              image: AssetImage('images/501_legion.png'),
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {},
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.all(5),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            image: DecorationImage(
+              image: AssetImage('images/187_th.png'),
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {},
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(5),
+          margin: const EdgeInsets.all(5),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            image: DecorationImage(
+              image: AssetImage('images/mystery_box.jpg'),
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {},
+          ),
+        ),
+      ],
     );
   }
 }
