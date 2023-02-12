65b38066129e458d2db8ad1d1e914fa69ba49a4e
Marcel <owczarek2000@gmail.com>
2023-02-12 21:19:30 +0100

    Add favoritebutton and pricetag to ShopPage items

--- lib/app/ShopPage.dart
+++ lib/app/ShopPage.dart
@@ -9,7 +9,7 @@ class ShopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 5,
+      length: 4,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.indigo,
@@ -43,45 +43,67 @@ class ShopPage extends StatelessWidget {
         ),
         body: (GridView.count(
           crossAxisCount: 2,
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(20),
           children: [
             Container(
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.all(5),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                image: DecorationImage(
-                  image: AssetImage('images/501_legion.png'),
-                ),
-              ),
-              child: GestureDetector(
-                onTap: () {},
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.all(5),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                image: DecorationImage(
-                  image: AssetImage('images/187_th.png'),
-                ),
-              ),
-              child: GestureDetector(
-                onTap: () {},
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.all(5),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                image: DecorationImage(
-                  image: AssetImage('images/mystery_box.jpg'),
-                ),
-              ),
-              child: GestureDetector(
-                onTap: () {},
+              margin: const EdgeInsets.only(top: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Stack(
+                    children: [
+                      Container(
+                        width: 160,
+                        height: 160,
+                        padding: const EdgeInsets.all(0),
+                        margin: const EdgeInsets.all(0),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                          image: DecorationImage(
+                            image: AssetImage('images/501_legion.png'),
+                          ),
+                        ),
+                        child: GestureDetector(
+                          onTap: () {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 160,
+                        height: 160,
+                        child: Align(
+                          alignment: const FractionalOffset(1.03, -0.03),
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.favorite_border_outlined,
+                              size: 20,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 160,
+                        height: 160,
+                        child: Align(
+                          alignment: const FractionalOffset(0.07, 0.93),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            color: Colors.indigo,
+                            child: const Text(
+                              ' 35 zł ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
